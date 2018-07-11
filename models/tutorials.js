08a0c9c034252eca5883612d1825ed384d7634e1
Goeff888 <33512526+Goeff888@users.noreply.github.com>
2018-07-11 21:19:33 +0200

    Produktivsetzung

--- models/tutorials.js
+++ models/tutorials.js
@@ -4,6 +4,7 @@ var tutorialsSchema = new mongoose.Schema({
   name: String,
   category:       {type: String, default: 'uncategorized'},//Kurs, Tutorial, Projekt, textur, objekte
   url:          {type: String, default: 'n.a'},
-  format:         {type: String, default: 'not set'} //pdf, video, 
+  format:         {type: String, default: 'not set'}, //pdf, video,
+  workfield:      {type: String, default: 'not set'} // Digitale Kunst, 3D, Composing, Lighting, Charakter, Animation,...
 });
 module.exports = mongoose.model("tutorials", tutorialsSchema);
